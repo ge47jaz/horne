ad7a63105338ab73f2609d192366f6a2a38798d5
ge47jaz <ge47jaz@mytum.de>
2023-10-05 00:19:56 +0200

    added animation to back to top, fixed interaction issues

--- site/src/jsMain/kotlin/com/chris/horne/components/BackToTopButton.kt
+++ site/src/jsMain/kotlin/com/chris/horne/components/BackToTopButton.kt
@@ -2,6 +2,7 @@ package com.chris.horne.components
 
 import androidx.compose.runtime.*
 import com.chris.horne.models.Theme
+import com.chris.horne.styles.BackToTopStyle
 import com.varabyte.kobweb.compose.css.Cursor
 import com.varabyte.kobweb.compose.css.Visibility
 import com.varabyte.kobweb.compose.foundation.layout.Arrangement
@@ -11,8 +12,11 @@ import com.varabyte.kobweb.compose.ui.Alignment
 import com.varabyte.kobweb.compose.ui.Modifier
 import com.varabyte.kobweb.compose.ui.graphics.Colors
 import com.varabyte.kobweb.compose.ui.modifiers.*
+import com.varabyte.kobweb.compose.ui.styleModifier
 import com.varabyte.kobweb.silk.components.icons.fa.FaArrowUp
 import com.varabyte.kobweb.silk.components.icons.fa.IconSize
+import com.varabyte.kobweb.silk.components.style.breakpoint.Breakpoint
+import com.varabyte.kobweb.silk.components.style.toModifier
 import com.varabyte.kobweb.silk.theme.breakpoint.rememberBreakpoint
 import kotlinx.browser.document
 import kotlinx.browser.window
@@ -34,31 +38,37 @@ fun BackToTopButton() {
         modifier = Modifier
             .fillMaxSize()
             .position(Position.Fixed) // back to top button must remain at same place
-            .zIndex(1), // make sure that button is placed on top of all elements
+            .zIndex(1) // make sure that button is placed on top of all elements
+            .styleModifier {
+                property("pointer-events", "none")
+            },// without this, only back to top button clickable sinch on top of every other component (add below aswell!)
         verticalArrangement = Arrangement.Bottom,
         horizontalAlignment = Alignment.End
     ) {
         Box(
-            modifier = Modifier
-                .size(50.px)
+            modifier = BackToTopStyle.toModifier()
+                .size(if (breakpoint <= Breakpoint.SM) 30.px else 50.px)
                 .visibility(  // calculated based on current scroll value
                     if (scroll != null && scroll!! > 400) Visibility.Visible else Visibility.Hidden
                 )
                 .borderRadius(10.px)
                 .margin(
-                    right = 40.px,
-                    bottom = 40.px
+                    right = if (breakpoint <= Breakpoint.SM) 20.px else 40.px,
+                    bottom = if (breakpoint <= Breakpoint.SM) 20.px else 40.px
                 )
                 .backgroundColor(Theme.Primary.rgb)
                 .cursor(Cursor.Pointer)
                 .onClick {
                     document.documentElement?.scroll(x = 0.0, y = 0.0)
-                },
+                }
+                .styleModifier {
+                    property("pointer-events", "auto") // same as top, only with auto
+                }, // -> button becomes clickable
             contentAlignment = Alignment.Center
         ) {
             FaArrowUp(
                 modifier = Modifier.color(Colors.White),
-                size = IconSize.LG
+                size = if (breakpoint <= Breakpoint.SM) IconSize.SM else IconSize.LG
             )
         }
     }
